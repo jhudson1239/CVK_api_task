5221f89cf9e2e718f2297dc73d4063a0c9b27f4d
Joshua Hudson <hudo1239@gmail.com>
2018-07-22 19:59:19 +0100

    Update section comeplte

---
+++ api/book/update.php
@@ -0,0 +1,37 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Book.php';
+
+    //Instantiate DB and Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate blog post object
+    $book = new Book($db);
+
+    // GET raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    //Set ID to update
+    $book->id = $data->id;
+    $book->title = $data->title;
+    $book->description = $data->description;
+    $book->author_id = $data->author_id;
+    $book->genre_id = $data->genre_id;
+    $book->price = $data->price;
+
+    if($book->update()){
+        echo json_encode(
+            array('message' => 'Book Updated')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Book Not Updated')
+        );
+    }
--- models/Book.php
+++ models/Book.php
@@ -92,4 +92,44 @@
             $this->genre_genre = $row['genre_genre'];
             
         }
+
+        public function update(){
+            //Create query
+            $query = 'UPDATE ' . 
+                $this->table . '
+            SET
+                title = :title,
+                description = :description,
+                author_id = :author_id,
+                genre_id = :genre_id,
+                price = :price
+            WHERE
+                id = :id;'; 
+
+            //Prepare statment
+            $stmt = $this->conn->prepare($query);
+
+            //clean data
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->description = htmlspecialchars(strip_tags($this->description));
+            $this->author_id = htmlspecialchars(strip_tags($this->author_id));
+            $this->genre_id = htmlspecialchars(strip_tags($this->genre_id));
+            $this->price = htmlspecialchars(strip_tags($this->price));
+
+            //bind data
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':description', $this->description);
+            $stmt->bindParam(':author_id', $this->author_id);
+            $stmt->bindParam(':genre_id', $this->genre_id);
+            $stmt->bindParam(':price', $this->price);
+            $stmt->bindParam(':id', $this->id);
+
+
+            if($stmt->execute()){
+                return true;
+            } else {
+                printf("Error: %s.\n", $stmt->error);
+                return false;
+            }
+        }
     }
