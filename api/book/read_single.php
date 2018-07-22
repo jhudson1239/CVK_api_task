834c4e0e18154819f3eee5d2d9248e041995b72e
Joshua Hudson <hudo1239@gmail.com>
2018-07-22 18:51:45 +0100

    sinlge read created and working

---
+++ api/book/read_single.php
@@ -0,0 +1,32 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Book.php';
+
+    //Instantiate DB and Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate blog post object
+    $book = new Book($db);
+
+    //Get ID from URL
+    $book->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    $book->read_single();
+
+    //Create array
+    $book_arr = array(
+        'id' => $book->id,
+        'title' => $book->title,
+        'price' => $book->price,
+        'author_name' => $book->author_name,
+        'genre_genre' => $book->genre_genre,
+        'description' => $book->description
+    );
+
+    //Make JSON
+    print_r(json_encode($book_arr));
--- models/Book.php
+++ models/Book.php
@@ -48,4 +48,48 @@
 
             return $stmt;
         }
+
+        //Get single
+        public function read_single(){
+             //Create query
+             $query = 'SELECT
+                author.name as author_name,
+                genre.genre as genre_genre,
+                books.id,
+                books.title,
+                books.price,
+                books.author_id,
+                books.genre_id,
+                books.description
+            FROM
+                ' . $this->table . '
+            LEFT JOIN 
+                author ON books.author_id = author.id
+            LEFT JOIN
+                genre ON books.genre_id = genre.id
+            WHERE
+                books.id = ?';
+            
+            // Prepare statment            
+            $stmt = $this->conn->prepare($query);
+
+            //Bind Id
+            $stmt->bindParam(1, $this->id);
+
+            //Execute query
+            $stmt->execute();
+            
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            //Set propertise
+            $this->id = $row['id'];
+            $this->title = $row['title'];
+            $this->description = $row['description'];
+            $this->price = $row['price'];
+            $this->author_id = $row['author_id'];
+            $this->author_name = $row['author_name'];
+            $this->genre_id = $row['genre_id'];
+            $this->genre_genre = $row['genre_genre'];
+            
+        }
     }
